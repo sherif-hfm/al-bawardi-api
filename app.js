c868cdb5a1897e9b2abca0b21e5cc667929f5099
Sherif Hamdy <sherif_hfm@yahoo.com>
2023-08-07 21:37:46 +0300

    updates

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var mysql = require('mysql2');
+//////////////////
 const path=require('path');
 const express=require('express');
 const helmet = require("helmet");
@@ -17,6 +19,35 @@ const prayer = require('./models/prayer');
 const user = require('./models/user');
 
 const app=express();
+
+app.get("/test", function (req, res) {
+  res.send("NodeJS app running : " + (new Date()).toString());
+});
+
+app.get("/testdb", function (req, res) {
+  sequelize.authenticate().then(result=>{
+    res.send("sequelize ok : " + (new Date()).toString());
+    
+  }).catch(err=>{
+    res.send("sequelize error : " + (new Date()).toString() + err);
+    console.error(err); 
+  });  
+});
+
+app.get("/testdb2", function (req, res) {
+  var con = mysql.createConnection({
+    host: process.env.MY_SQL_HOST,
+    user: process.env.MY_SQL_USER,
+    password: process.env.MY_SQL_PWD,
+    database: process.env.MY_SQL_DB
+  });  
+  con.connect(function(err) {
+    if (err) 
+      res.send("mysql error : " + (new Date()).toString() + err);
+    else
+      res.send("Connected!");
+  });  
+});
 
 app.use(nocache());
 app.use(helmet());
@@ -74,32 +105,32 @@ app.use((error, req, res, next) => {
   });
   
 
-
-
   sequelize.authenticate().then(result=>{
     //sequelize.sync({force:true})
-    //sequelize.sync();
-    process.env.TZ = "Asia/Kuwait";
-    console.warn('process.env');
-	console.warn(process.env);
-	  
-    // console.log(process.env.TZ);
-    //
-    console.log('Date:' + (new Date()).toString());
-    if(process.env.NODE_ENV=='production'){
-      var server = app.listen(process.env.PORT, function () {
-        var host = server.address().address
-        var port = server.address().port
-        console.log("Example app listening at http://%s:%s", host, port)
-     })
-    }
-    else{
-      app.listen(process.env.PORT);
-    }
+    //sequelize.sync();  
+    console.log('sequelize OK');  
     
   }).catch(err=>{
-    console.log(err); 
+    console.error('sequelize error');
+    console.error(err); 
   });
+
+  if(process.env.NODE_ENV=='production'){
+    var server = app.listen(process.env.PORT, function () {
+      var host = server.address().address
+      var port = server.address().port
+      process.env.TZ = "Asia/Kuwait";
+      console.warn('process.env');
+      console.warn(process.env);      
+      // console.log(process.env.TZ);      
+      console.log("Example app listening at http://%s:%s", host, port)
+   })
+  }
+  else{
+    app.listen(process.env.PORT);
+  }
+
+ 
   
 
  
